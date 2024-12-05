14d886cc443106c2c2fab6180871544d4bbfd7db
adhl-0216 <hanlim2002@gmail.com>
2024-12-05 02:45:43 +0000

    jest config

--- jest.config.ts
+++ jest.config.ts
@@ -1,23 +1,32 @@
-import { Config } from 'jest';
+import type { Config } from 'jest';
 
 const config: Config = {
-    preset: 'ts-jest', // Use ts-jest to transform TypeScript files
+    preset: 'ts-jest',
     testEnvironment: 'jsdom',
     moduleNameMapper: {
-        // Handle CSS imports (e.g., CSS Modules)   
         '^.+\\.module\\.(css|sass|scss)$': 'identity-obj-proxy',
-        // Handle static assets
         '^.+\\.(jpg|jpeg|png|gif|webp|svg|eot|otf|ttf|woff|woff2)$': '<rootDir>/__mocks__/fileMock.js',
-        // Handle absolute imports and aliases
         '^@/(.*)$': '<rootDir>/src/$1',
     },
-    setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'], // Extend Jest functionality
-    testPathIgnorePatterns: ['<rootDir>/.next/', '<rootDir>/node_modules/'], // Ignore unnecessary paths
+    setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
+    testPathIgnorePatterns: ['<rootDir>/.next/', '<rootDir>/node_modules/'],
     transform: {
-        '^.+\\.(ts|tsx)$': 'ts-jest', // Transform TypeScript files
-        '^.+\\.(js|jsx)$': 'babel-jest', // Use Babel for JavaScript files
+        '^.+\\.(ts|tsx)$': ['ts-jest', {
+            tsconfig: 'tsconfig.jest.json', // Create this file
+        }],
+        '^.+\\.(js|jsx)$': ['babel-jest', {
+            presets: ['next/babel'], // Use Next.js babel preset
+        }],
     },
-    transformIgnorePatterns: ['/node_modules/'],
+    transformIgnorePatterns: [
+        '/node_modules/',
+        '^.+\\.module\\.(css|sass|scss)$',
+    ],
+    globals: {
+        'ts-jest': {
+            tsconfig: 'tsconfig.jest.json',
+        },
+    },
 };
 
 export default config;
